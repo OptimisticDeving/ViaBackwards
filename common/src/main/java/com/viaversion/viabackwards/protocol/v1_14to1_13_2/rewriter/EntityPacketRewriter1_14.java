5615d0591fe204d86cd7fcde5d0bc76cd93fda66
Nassim Jahnke <nassim@njahnke.dev>
2026-09-04 21:04:57 +0200

    Remove 1.14->1.13 cat name

--- common/src/main/java/com/viaversion/viabackwards/protocol/v1_14to1_13_2/rewriter/EntityPacketRewriter1_14.java
+++ common/src/main/java/com/viaversion/viabackwards/protocol/v1_14to1_13_2/rewriter/EntityPacketRewriter1_14.java
@@ -539,7 +539,7 @@ public class EntityPacketRewriter1_14 extends LegacyEntityRewriter<ClientboundPa
     @Override
     public void onMappingDataLoaded() {
         super.onMappingDataLoaded();
-        mapEntityTypeWithData(EntityTypes1_14.CAT, EntityTypes1_14.OCELOT).jsonName();
+        mapEntityTypeWithData(EntityTypes1_14.CAT, EntityTypes1_14.OCELOT);
         mapEntityTypeWithData(EntityTypes1_14.TRADER_LLAMA, EntityTypes1_14.LLAMA).jsonName();
         mapEntityTypeWithData(EntityTypes1_14.FOX, EntityTypes1_14.WOLF).jsonName();
         mapEntityTypeWithData(EntityTypes1_14.PANDA, EntityTypes1_14.POLAR_BEAR).jsonName();
